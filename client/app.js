7c3ac9838a59c6fe885a7d3847a0a6350a1a301b
Danny Blue <dblue@deloitte.com>
2015-01-11 15:53:37 -0500

    add application controller and add title to each state

--- client/app.js
+++ client/app.js
@@ -30,18 +30,21 @@ angular
         url: '',
         templateUrl: 'app/views/main/main.html',
         controller: 'MainCtrl',
-        controllerAs: 'main'
+        controllerAs: 'main',
+        title: 'My App'
       })
       .state('main.home', {
         url: '/',
         templateUrl: 'app/views/home/home.html',
         controller: 'HomeCtrl',
-        controllerAs: 'home'
+        controllerAs: 'home',
+        title: 'Home'
       })
       .state('main.about', {
         url: '/about',
         templateUrl: 'app/views/about/about.html',
         controller: 'AboutCtrl',
-        controllerAs: 'about'
+        controllerAs: 'about',
+        title: 'About'
       });
   });
