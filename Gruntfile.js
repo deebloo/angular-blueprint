8f153e5e7e907fd67f4d84e430daed5be487a6d3
Danny Blue <dblue@deloitte.com>
2015-01-11 17:46:24 -0500

    remove un-needed tasks from grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -280,24 +280,6 @@ module.exports = function (grunt) {
           ]
         }
       }
-
-      // Inject component css into index.html
-      //css: {
-      //  options: {
-      //    transform: function(filePath) {
-      //      filePath = filePath.replace('/client/', '');
-      //      filePath = filePath.replace('/.tmp/', '');
-      //      return '<link rel="stylesheet" href="' + filePath + '">';
-      //    },
-      //    starttag: '<!-- injector:css -->',
-      //    endtag: '<!-- endinjector -->'
-      //  },
-      //  files: {
-      //    '<%= yeoman.client %>/index.html': [
-      //      '<%= yeoman.client %>/{app,components}/**/*.css'
-      //    ]
-      //  }
-      //}
     },
 
     /**
