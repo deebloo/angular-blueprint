7db5c4588503006dc31cf085e4bcf892266616b2
Danny Blue <dannyblue@LiveSafes-MacBook-Pro.local>
2016-04-25 20:07:28 -0400

    tweak files

--- build/FILES.js
+++ build/FILES.js
@@ -5,14 +5,20 @@ const srcDir = (() => {
 	let src;
 
 	try {
-		src = require('../.yo-rc.json')['generator-angular-blueprint'].appDir;
-	}
-	catch (e) {
+		const yorc = require('../.yo-rc.json')['generator-angular-blueprint'];
+		
+		if(yorc.appDir) {
+			src = yorc.appDir;	
+		} else {
+			src = 'src';
+		}
+	} catch (e) {
 		src = 'src';
 	}
 
 	return src;
 })();
+
 const appDir = path.join(srcDir, '/app');
 const src = path.join(__dirname, '/../', srcDir);
 const app = path.join(src, '/app');
